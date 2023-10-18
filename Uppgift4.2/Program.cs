903b3932d60df46fdc068ead9601467c6e3a7e46
arun06001 <arun06001@E-PW0234KE>
2023-10-18 12:33:48 +0200

    Add project files.

---
+++ Uppgift4.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Uppgift4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxTal = 0;
+            bool cont = true;
+            int svar;
+
+            do
+            {
+                Console.WriteLine("Skriv in ett heltal");
+                svar = int.Parse(Console.ReadLine());
+
+                if (svar > maxTal)
+                {
+                    maxTal = svar;
+                }
+
+                Console.WriteLine("Vill du skriva in ett heltal till?");
+                Console.WriteLine("[J]a");
+                Console.WriteLine("[N]ej");
+                if (Console.ReadLine().ToLower() == "n") //kollar om de sa nej och avslutar loopen då
+                {
+                    cont = false;
+                }
+            } while (cont);
+
+            Console.WriteLine($"Det största talet du skrev var {maxTal}.");
+
+            Console.WriteLine("Tryck på valfri tangent för att avsluta");
+            Console.ReadKey();
+        }
+    }
+}
